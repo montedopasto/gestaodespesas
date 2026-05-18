7a6a54c1f15b5ec946c73bde27d399eda0828848
montedopasto <mdopasto@gmail.com>
2026-05-18 11:54:04 +0100

    Update despesas.js

--- js/despesas.js
+++ js/despesas.js
@@ -937,10 +937,11 @@ window.downloadPDF = async function(){
             }
 
             th{
-                background:#2e7d32;
-                color:white;
-                padding:8px;
-            }
+    background:#2e7d32;
+    color:white;
+    padding:5px;
+    font-size:11px;
+}
 
             td{
                 border:1px solid #ccc;
